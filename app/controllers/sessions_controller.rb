72ab189663700d85d2b5e33c9aef92f6d8f83a9e
Ryoichi Gokan <nargok@gmail.com>
2018-09-01 11:55:37 +0900

    ログインフォームのエラーメッセージ出力を実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,23 @@
 class SessionsController < ApplicationController
   def new
     @form = LoginForm.new
+    @errors = []
   end
 
   def create
+    @errors = []
     @form = LoginForm.new(login_params)
     user = User.find_by(user_id: params[:login_form][:user_id])
     if user && user.password == params[:login_form][:password]
       session[:user_id] = user.id
       redirect_to root_path
     else
-      @errors = []
-      if params[:login_form][:user_id].nil?
-        error = "ユーザーIDを入力してください"
-        @errors << error
+      @errors << "ユーザーID、パスワードが一致するアカウントが見つかりません"
+      if params[:login_form][:user_id].blank?
+        @errors << "ユーザーIDを入力してください"
       end
-      if params[:login_form][:password].nil?
-        error = "パスワードを入力してください"
-        @errors << error
+      if params[:login_form][:password].blank?
+        @errors << "パスワードを入力してください"
       end
       render :new
     end
