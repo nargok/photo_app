5694bb7f89ef59f81974427c56ad5977557efcd2
Ryoichi Gokan <nargok@gmail.com>
2018-09-02 14:24:56 +0900

    access_tokenの取得処理を実装

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,6 @@
 class PhotosController < ApplicationController
+  require 'net/http'
+
   before_action :require_login
   before_action :set_photo, only: [:new, :create]
 
@@ -26,6 +28,31 @@ class PhotosController < ApplicationController
     else
       render :new
     end
+  end
+
+  def callback
+    code = params[:code]
+
+    # パラメータは一度素でここで作成する　→　後でconstants化する
+    # パラメータを作成する
+    params = {
+      'client_id' => 'f17620f69de54893c5df06796da7271e0fa6911e318bff0b8ea54a7cce7980ce',
+      'client_secret' => 'e3140d157ced3fbbc2900bd64ad4f93b1a0ae034174a89ce1844e3ad61715d4e',
+      'redirect_uri' => 'http://localhost:3000/oauth/callback',
+      'grant_type' => 'authorization_code',
+      'code' => code
+    }
+
+    # http通信する
+    res = Net::HTTP.post_form(URI.parse('https://arcane-ravine-29792.herokuapp.com/oauth/token'), params)
+    api_response = JSON.parse(res.body)
+    token = api_response['access_token']
+
+    # 取得したアクセストークンをsessionに追加する
+    session[:tweet_app_token] =  token
+
+    # 写真一覧へリダイレクトする
+    redirect_to photos_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'photos/index'
   get 'login', to: 'sessions#new'
   delete 'logout', to: 'sessions#destroy'
+  get 'oauth/callback', to: 'photos#callback'
 
   resource :sessions, only: [:new, :create, :destroy]
   resources :photos, only: [:index, :new, :create]
